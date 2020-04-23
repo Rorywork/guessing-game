dadefba70bded867ff98f075009f8b7257f31e60
Rorywork <rorywork95@outlook.com>
2020-04-23 17:35:54 +0100

    Added focus to the guessField

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+guessField.focus();
 
 function checkGuess() {
     let userGuess = Number(guessField.value);
